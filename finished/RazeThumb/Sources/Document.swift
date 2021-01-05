8aabc1d399171da545fb98b2b7bfe6f902a6d952
Chuck Krutsinger <chuck.krutsinger@countermind.com>
2021-01-04 17:31:03 -0700

    remove unused imports

--- finished/RazeThumb/Sources/Document.swift
+++ finished/RazeThumb/Sources/Document.swift
@@ -30,8 +30,6 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import UIKit.UIScreen
-import QuickLookThumbnailing
 import QuickLook
 
 struct Document: Hashable {
